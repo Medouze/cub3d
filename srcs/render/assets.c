1cee626b4dd1fa890373218acc1dd0d017298db7
Quentin Morineau <qmorinea@fu-r6-p1.s19.be>
2025-04-14 16:39:57 +0200

    fixed texture loaded

--- srcs/render/assets.c
+++ srcs/render/assets.c
@@ -6,7 +6,7 @@
 /*   By: qmorinea <qmorinea@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 16:04:25 by qmorinea          #+#    #+#             */
-/*   Updated: 2025/04/14 16:13:11 by qmorinea         ###   ########.fr       */
+/*   Updated: 2025/04/14 16:38:58 by qmorinea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,22 @@ static t_img	init_img(void)
 	return (img);
 }
 
-static void	load_sprite(t_config *config, t_game *game, char *path)
+static void	load_sprite(t_game *game, t_img *img, char *path)
 {
 	int		height;
 	int		width;
-	t_img	data;
 
 	height = 0;
 	width = 0;
 	printf("sprite = %s\n", path);
-	data.img = mlx_xpm_file_to_image(game->mlx.mlx_ptr, path, &width, &height);
-	if (!data.img)
-		print_error(".xpm file doesn't exist.", config);
-	data.add =  mlx_get_data_addr(data.img, &data.bpp, &data.size_line, &data.endians);
-	if (!data.add)
-		print_error("Function 'mlx_get_data_addr()' failed.", config);
-	data.height = height;
-	data.width = width;
+	img->img = mlx_xpm_file_to_image(game->mlx.mlx_ptr, path, &width, &height);
+	if (!img->img)
+		print_error(".xpm file doesn't exist.", game->config);
+	img->add =  mlx_get_data_addr(img->img, &img->bpp, &img->size_line, &img->endians);
+	if (!img->add)
+		print_error("Function 'mlx_get_data_addr()' failed.", game->config);
+	img->height = height;
+	img->width = width;
 }
 
 t_game init_assets(t_game *game)
@@ -47,11 +46,11 @@ t_game init_assets(t_game *game)
 	game->east = init_img();
 	game->door = init_img();
 	game->floor_ceil = init_img();
-	load_sprite(game->config, game, game->config->no_texture);
-	load_sprite(game->config, game, game->config->so_texture);
-	load_sprite(game->config, game, game->config->we_texture);
-	load_sprite(game->config, game, game->config->ea_texture);
+	load_sprite(game, &game->north, game->config->no_texture);
+	load_sprite(game, &game->south, game->config->so_texture);
+	load_sprite(game, &game->west, game->config->we_texture);
+	load_sprite(game, &game->east, game->config->ea_texture);
 	if (game->config->door_texture)
-		load_sprite(game->config, game, game->config->door_texture);
+		load_sprite(game, &game->door, game->config->door_texture);
 	return (*game);
 }
